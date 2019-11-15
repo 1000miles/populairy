27fd51014b05a58a5025a4f2be07af20e813eb73
Thuy Le <gitnap@mailbox.org>
2019-11-15 18:55:40 +0100

    Check in prettify formatting

--- week-2-async/database.js
+++ week-2-async/database.js
@@ -1,24 +1,24 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // FIXME: database-new.json => undefined
-const save = function (filename, data) {
-	console.log(`DATA:`, data)
-	fs.writeFile(filename, JSON.stringify(data), (err, data) => {
-		if (err) throw err
-		console.log(data)
-	})
-}
+const save = function(filename, data) {
+  console.log(`DATA:`, data);
+  fs.writeFile(filename, JSON.stringify(data), (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+};
 
 const load = function(filename, handler) {
-  fs.readFile(filename, 'utf8', (err, file) => {
+  fs.readFile(filename, "utf8", (err, file) => {
     if (err) {
-      console.log('there is a read error', err)
-      handler(err)
-      return
+      console.log("there is a read error", err);
+      handler(err);
+      return;
     }
 
     handler(null, JSON.parse(file));
-  })
-}
+  });
+};
 
-module.exports = { save, load }
+module.exports = { save, load };
--- week-2-async/models/Popup.js
+++ week-2-async/models/Popup.js
@@ -1,18 +1,18 @@
-const Chalk = require("chalk")
+const Chalk = require("chalk");
 
 module.exports = class Popup {
   constructor(category, title, host, space, date) {
-    this.category = category
-    this.title = title
-    this.host = host
-    this.space = space
-    this.date = date
-    this.attendees = []
+    this.category = category;
+    this.title = title;
+    this.host = host;
+    this.space = space;
+    this.date = date;
+    this.attendees = [];
   }
 
   printAttendeesNames() {
     // this.attendees.push(printHost, printGuest)
-    console.log(`[printAttendeesNames]`, this.attendees)
+    console.log(`[printAttendeesNames]`, this.attendees);
   }
 
   getPopupInfo() {
@@ -27,19 +27,13 @@ module.exports = class Popup {
       Chalk.red(this.space),
       `.`,
       `It currently has ${this.attendees.length} people attending.`
-    )
-	}
+    );
+  }
 
-	static create({
-		category,
-		title,
-		host,
-		space,
-		date
-	}) {
-		return new Popup(category, title, host, space, date)
-	}
-}
+  static create({ category, title, host, space, date }) {
+    return new Popup(category, title, host, space, date);
+  }
+};
 
-const printGuest = (guest) => console.log(guest.name, guest.email)
-const printHost = (host) => console.log(host.name, host.email, host.phoneNumber)
+const printGuest = (guest) => console.log(guest.name, guest.email);
+const printHost = (host) => console.log(host.name, host.email, host.phoneNumber);
