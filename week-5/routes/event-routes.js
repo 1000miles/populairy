57548e9464383c5b54b37e30359e3fe860990d30
Thuy Le <gitnap@mailbox.org>
2019-11-27 20:33:01 +0100

    Prettify event routes and views

--- week-5/routes/event-routes.js
+++ week-5/routes/event-routes.js
@@ -9,112 +9,112 @@ const PopupService = require('../services/popup-service');
 
 // GET http://localhost:3000/event/all
 router.get('/all', async (req, res) => {
-	try {
-		const events = await EventService.findAll();
-		const popups = await PopupService.findAll();
-		console.log(`[event-routes.js] events`, events);
+  try {
+    const events = await EventService.findAll();
+    const popups = await PopupService.findAll();
+    console.log(`[event-routes.js] events`, events);
 
-		res.render('events', { events, popups });
-	} catch (err) {
-		res.send(`Error while loading all events`);
-		console.log(err);
-	}
+    res.render('events', { events, popups });
+  } catch (err) {
+    res.send(`Error while loading all events`);
+    console.log(err);
+  }
 });
 
 // GET http://localhost:3000/event/all/json
 router.get('/all/json', async (req, res, next) => {
-	try {
-		const events = await EventService.findAll();
+  try {
+    const events = await EventService.findAll();
 
-		// res.render('eventlistJSON', { items: events });
+    // res.render('eventlistJSON', { items: events });
 
-		res.status(200).json({
-			status: 'Success.',
-			result: events.length,
-			data: events,
-		});
-	} catch (err) {
-		return next(err);
-	}
+    res.status(200).json({
+      status: 'Success.',
+      result: events.length,
+      data: events,
+    });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 // GET http://localhost:3000/event/ObjectId
 router.get('/:id', async (req, res) => {
-	try {
-		const event = await EventService.findById(req.params.id);
-		const location = event.location;
+  try {
+    const event = await EventService.findById(req.params.id);
+    const location = event.location;
 
-	//	console.log(`[event-routes.js] router.get`, event);
-	
-		res.render('event', { event, location });
-	} catch (err) {
-		res.status(404).send({ msg: `Error. Event does not exist.` });
-	}
+    //	console.log(`[event-routes.js] router.get`, event);
+
+    res.render('event', { event, location });
+  } catch (err) {
+    res.status(404).send({ msg: `Error. Event does not exist.` });
+  }
 });
 
 // GET http://localhost:3000/event/ObjectId/json
 router.get('/:id/json', async (req, res) => {
-	try {
-		const event = await EventService.findById(req.params.id);
+  try {
+    const event = await EventService.findById(req.params.id);
 
-		res.status(200).json({
-			status: 'Success.',
-			data: event,
-		});
-	} catch (err) {
-		res.status(404).json({
-			status: 'Error. Event does not exist.',
-			message: err,
-		});
-	}
+    res.status(200).json({
+      status: 'Success.',
+      data: event,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Error. Event does not exist.',
+      message: err,
+    });
+  }
 });
 
 // POST `/event/new` w/ req.body
 router.post(
-	'/new',
-	eventController.validate('createEvent'),
-	async (req, res, next) => {
-		try {
-			const errors = validationResult(req);
+  '/new',
+  eventController.validate('createEvent'),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
 
-			if (!errors.isEmpty()) {
-				return res.status(422).json({
-					status: 'Error. Event not created.',
-					errors: errors.array(),
-				});
-			}
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
+          status: 'Error. Event not created.',
+          errors: errors.array(),
+        });
+      }
 
-			const {
-				eventType,
-				eventName,
-				location,
-				date,
-				eventHost,
-				joinedHosts,
-				popups,
-				guests,
-			} = req.body;
+      const {
+        eventType,
+        eventName,
+        location,
+        date,
+        eventHost,
+        joinedHosts,
+        popups,
+        guests,
+      } = req.body;
 
-			// Create a new event with required and optional values
-			const event = await EventService.add({
-				eventType,
-				eventName,
-				location,
-				date,
-				eventHost,
-				joinedHosts,
-				popups,
-				guests,
-			});
+      // Create a new event with required and optional values
+      const event = await EventService.add({
+        eventType,
+        eventName,
+        location,
+        date,
+        eventHost,
+        joinedHosts,
+        popups,
+        guests,
+      });
 
-			await res.status(200).json({
-				status: 'Success. Event created.',
-				data: event,
-			});
-		} catch (err) {
-			return next(err);
-		}
-	},
+      await res.status(200).json({
+        status: 'Success. Event created.',
+        data: event,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
 );
 
 /**
@@ -124,56 +124,56 @@ router.post(
  */
 
 router.patch(
-	'/:id',
-	eventController.validate('updateEvent'),
-	async (req, res, next) => {
-		try {
-			const errors = validationResult(req);
+  '/:id',
+  eventController.validate('updateEvent'),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
 
-			if (!errors.isEmpty()) {
-				return res.status(422).json({
-					errors: errors.array(),
-					status: 'Error. Event not updated.',
-				});
-			} else {
-				const updatedEvent = await EventService.findByIdAndUpdate(
-					req.params.id,
-					req.body,
-					{
-						new: true,
-						runValidators: true,
-					},
-				);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
+          errors: errors.array(),
+          status: 'Error. Event not updated.',
+        });
+      } else {
+        const updatedEvent = await EventService.findByIdAndUpdate(
+          req.params.id,
+          req.body,
+          {
+            new: true,
+            runValidators: true,
+          },
+        );
 
-				await updatedEvent.save();
+        await updatedEvent.save();
 
-				console.log(`updatedEvent`, updatedEvent);
+        console.log(`updatedEvent`, updatedEvent);
 
-				return res.status(201).json({
-					status: 'Success. Event updated.',
-					data: updatedEvent,
-				});
-			}
-		} catch (err) {
-			return next(err);
-		}
-	},
+        return res.status(201).json({
+          status: 'Success. Event updated.',
+          data: updatedEvent,
+        });
+      }
+    } catch (err) {
+      return next(err);
+    }
+  },
 );
 
 // DELETE `/event/:id` JSON
 router.delete('/:id', async (req, res) => {
-	try {
-		const event = await EventService.deleteOne(req.params.id);
-		res.status(201).json({
-			status: 'Success. Event deleted.',
-			data: event,
-		});
-	} catch (err) {
-		res.status(422).json({
-			status: 'Fail. Event not deleted.',
-			message: err,
-		});
-	}
+  try {
+    const event = await EventService.deleteOne(req.params.id);
+    res.status(201).json({
+      status: 'Success. Event deleted.',
+      data: event,
+    });
+  } catch (err) {
+    res.status(422).json({
+      status: 'Fail. Event not deleted.',
+      message: err,
+    });
+  }
 });
 
 module.exports = router;
