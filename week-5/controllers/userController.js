c580a383614a556c3f7c264df16d25d0dbb42131
Thuy Le <gitnap@mailbox.org>
2019-12-02 00:56:50 +0100

    User model-view-controller
    - validate user through express-validation helper
    - CRUD routes for user model
    - rename andd update user model
    - uncommennt or remove unused functions for now
      (will re-added later)

---
+++ week-5/controllers/userController.js
@@ -0,0 +1,62 @@
+const { body } = require("express-validator/check");
+require("../models/with-mongoose/User");
+
+// User Schema Validator
+exports.validate = method => {
+  switch (method) {
+    case "createUser": {
+      return [
+        body("firstName", "First name can't be blank")
+				.isString()
+				.exists(),
+        body("lastName", "Last name can't be blank")
+				.isString()
+				.exists(),
+        body("email", "Email can't be blank")
+				.isString()
+				.exists(),
+				body("role")
+				.isString()
+				.exists(),
+				body("phoneNumber",
+					"Phone number must be a string.",
+        )
+				.isString()
+				.optional(),
+				body("events", "Events must be an array of strings.")
+				.isArray()
+				.optional(),
+				body("popups", "Pop-ups must be an array of strings.")
+				.isArray()
+				.optional(),
+      ];
+		}
+		case "updateUser": {
+      return [
+        body("firstName", "First name must be a string.")
+				.isString()
+				.optional(),
+        body("lastName", "Last name must be a string.")
+				.isString()
+				.optional(),
+        body("email", "Email can't be blank")
+				.isString()
+				.optional(),
+				body("role")
+				.isString()
+				.optional(),
+				body("phoneNumber",
+					"Phone number must be a string.",
+        )
+				.isString()
+				.optional(),
+				body("events", "Events must be an array of strings.")
+				.isArray()
+				.optional(),
+				body("popups", "Pop-ups must be an array of strings.")
+				.isArray()
+				.optional(),
+      ];
+    }
+  }
+};
--- week-5/services/user-service.js
+++ week-5/services/user-service.js
@@ -1,15 +1,13 @@
 const Chalk = require("chalk");
 const BaseService = require("./base-service");
-const User = require("../models/with-mongoose/UserNEW");
+const User = require("../models/with-mongoose/User");
 
 class UserService extends BaseService {
   model = User;
 
   async attendEvent(user, event) {
-    console.log(`DEBUG [user-service.js] event 1:`, event);
-    // console.log(`DEBUG [user-service.js] user 1:`, user);
-
-    // FIXME: UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'push' of undefined
+		console.log(`DEBUG [user-service.js] event 1:`, event);
+		
     user = `${user.firstName} ${user.lastName}`;
     console.log(`[user-service.js] user`, user);
 
@@ -18,15 +16,6 @@ class UserService extends BaseService {
 
     await user.save();
     await event.save();
-  }
-
-  async getUserInfo(user) {
-    const message1 = `The ${user.role} ${user.firstName} ${user.lastName} can be contacted via ${user.email} and ${user.phoneNumber}.`;
-
-    const message2 = `The ${user.role} ${user.firstName} ${user.lastName} can be contacted via ${user.email}.`;
-
-    if (user.role === "host" || user.role === "organizer") return message1;
-    else return message2;
   }
 }
 
